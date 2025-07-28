eca10014adebd3cbb89fc566fee1115348eaeda1
nisargagowda2004 <nisargagowda2004@gmail.com>
2025-07-28 21:56:35 +0530

    commit 1

--- Git/Assignment/new.c
+++ Git/Assignment/new.c
@@ -4,11 +4,13 @@ return 0;
 }
 void main()
 {
-	<<<<<<<< HEAD
-	//int main=90;
+	
 	//this is from main
 	=======
 	//int ab=0;
 	printf("addressed review comment");
-	>>>>>>>> Dev
+}
+int main()
+{
+	double d=0.0;
 }
